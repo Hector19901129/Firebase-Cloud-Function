9ff390e6a7163da464405d44ef2f32d7ad4a340a
Helga Sokolova <helga.sokolova130@gmail.com>
2019-03-12 02:06:47 +0800

    Use timestamp from devices instead of server

--- functions/exports/markGroupConversationAsRead.js
+++ functions/exports/markGroupConversationAsRead.js
@@ -3,11 +3,11 @@ const admin = require('firebase-admin');
 
 exports = module.exports = functions.https.onCall((data, context) => {
     const senderId = context.auth.uid;
-    const { isStudent, groupId } = data;
+    const { isStudent, groupId, timestamp } = data;
 
     let userType = isStudent ? `students` : `faculty`;
 
-    return admin.database().ref(`${userType}/${senderId}/groups/${groupId}`).update({ lastSeen: new Date(), unreads: 0 })
+    return admin.database().ref(`${userType}/${senderId}/groups/${groupId}`).update({ lastSeen: timestamp, unreads: 0 })
       .then((snapshot) => {
           return { result: 'Success' };
       })
